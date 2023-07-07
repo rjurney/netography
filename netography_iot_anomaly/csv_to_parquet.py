c38340fa7c1b5e03eba586b5e7938f1bd4b95636
Russell Jurney <russell.jurney@gmail.com>
2023-07-06 22:20:12 -0700

    Black reformatted CSV to Parquet

--- netography_iot_anomaly/csv_to_parquet.py
+++ netography_iot_anomaly/csv_to_parquet.py
@@ -3,12 +3,8 @@
 import pandas as pd
 
 # Read the CSV files
-test_df: pd.DataFrame = pd.read_csv(
-    "data/ml-engineer-main/IoT network intrustion dataset test.csv.gz"
-)
-train_df: pd.DataFrame = pd.read_csv(
-    "data/ml-engineer-main/IoT network intrustion dataset train.csv.gz"
-)
+test_df: pd.DataFrame = pd.read_csv("data/ml-engineer-main/IoT network intrustion dataset test.csv.gz")
+train_df: pd.DataFrame = pd.read_csv("data/ml-engineer-main/IoT network intrustion dataset train.csv.gz")
 
 # Convert the CSV files to Parquet format
 test_df.to_parquet("data/ml-engineer-main/iot_network_intrustion_dataset_test.parquet")
