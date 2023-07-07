e038583810f9ed7a25f855ca49db825b85258307
Russell Jurney <russell.jurney@gmail.com>
2023-07-06 22:13:19 -0700

    Working script to convert the project's Gzip CSV to Parquet format

---
+++ netography_iot_anomaly/csv_to_parquet.py
@@ -0,0 +1,15 @@
+# This file converts the assignment CSV files to Parquet format
+
+import pandas as pd
+
+# Read the CSV files
+test_df: pd.DataFrame = pd.read_csv(
+    "data/ml-engineer-main/IoT network intrustion dataset test.csv.gz"
+)
+train_df: pd.DataFrame = pd.read_csv(
+    "data/ml-engineer-main/IoT network intrustion dataset train.csv.gz"
+)
+
+# Convert the CSV files to Parquet format
+test_df.to_parquet("data/ml-engineer-main/iot_network_intrustion_dataset_test.parquet")
+train_df.to_parquet("data/ml-engineer-main/iot_network_intrustion_dataset_train.parquet")
